707c4c4ea40c2d39948a956fda78b4b7eee9906a
buriy40 <burenin40@gmail.com>
2024-09-20 12:41:30 +0300

    Create Strings.py

---
+++ Strings.py
@@ -0,0 +1,6 @@
+name = input("Введите Ваше имя: ")
+Age = int(input('Введите Ваш возраст, целых лет: '))
+current_year = 2024
+year_of_birth = current_year - Age
+print("Здравствуйте, ", name)
+print('Вы родились в' , year_of_birth, 'году')
